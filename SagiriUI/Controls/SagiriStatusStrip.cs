b65ee3f580abe131be6715c2205b9a297ec884bc
Asteriskx <masteriskxxx@gmail.com>
2021-07-24 18:02:31 +0900

    [wip] Update SagiriStatusStrip Class summary.

--- SagiriUI/Controls/SagiriStatusStrip.cs
+++ SagiriUI/Controls/SagiriStatusStrip.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace SagiriUI.Controls
 {
+    /// <summary>
+    /// !! WIP - Don't use. !!
+    /// </summary>
     public partial class SagiriStatusStrip : StatusStrip
     {
         public SagiriStatusStrip()
